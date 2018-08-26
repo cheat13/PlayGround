d3ce2119f66f64bc28968627ab51e5a1c05d5c44
Kritsana <tragoolphrom@gmail.com>
2018-08-26 19:57:12 +0700

    Create WebAPI & Data Access
    Signed-off-by: Kritsana <tragoolphrom@gmail.com>

---
+++ WebAPI/WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Authentication;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        IMongoCollection<Student> Collection { get; set; }
+
+        public HomeController()
+        {
+            var settings = MongoClientSettings.FromUrl(new MongoUrl("mongodb://krit_NA:thegigclubna2522@ds125322.mlab.com:25322/kritna"));
+            settings.SslSettings = new SslSettings()
+            {
+                EnabledSslProtocols = SslProtocols.Tls12
+            };
+            var mongoClient = new MongoClient(settings);
+            var database = mongoClient.GetDatabase("kritna");
+            Collection = database.GetCollection<Student>("orders2");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
